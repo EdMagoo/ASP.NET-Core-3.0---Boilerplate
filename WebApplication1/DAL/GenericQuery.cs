2d3de1d68a82b59b0197d2704290b202b5fd4189
Edwin <edwin_m_paz@hotmail.com>
2020-01-06 21:31:37 -0600

    Dynamic Queries - init commit

---
+++ WebApplication1/DAL/GenericQuery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.DAL
+{
+    /** the principal methods to be used by other classes in this class
+     * return an IQueryable<T>
+     *
+     * SQL like basic query structure:
+     * 
+     * SELECT [FUNCTION(entity1.property1)][,property2,...]
+     * FROM entity1
+     * [WHERE entity1.property1 =||<||>||<=||>=||!=||LIKE value1 [AND||OR] entity1.property2 =||<||>||<=||>=||!=||LIKE value2 ... ]
+     * [GROUP BY property2[,...]]
+     * [HAVING [FUNCTION(entity1.property1)] =||<||>||<=||>=||!=||LIKE value1 [AND||OR ...]]
+     * [ORDER BY entity1.property1 ASC||DESC[, ...]]
+     */
+    public abstract class GenericQuery<T, U, K>  
+        where T : class, IEntity
+        where U : struct
+        where K : DbContext
+    {
+        IQueryable<T> Query { get; set; }
+    
+        protected GenericQuery(K dbContext)
+        {
+            Query = dbContext.Set<T>().AsNoTracking();
+        }
+
+        protected IQueryable<T> BuildQuery(string includedProperties = "")
+        {
+            IQueryable<T> query = Query.Include(includedProperties);
+                
+            /*
+             * Initial operations
+             *
+             * Select
+             * Where
+             * Order by
+             */
+
+            return query;
+        }
+    }
+}
